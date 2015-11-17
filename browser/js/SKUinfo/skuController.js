53b48a56f5c2c3bc54843a2ab5e13683072ca0a8
Vivek Pandya <vivek.pandya93@gmail.com>
2015-11-17 11:59:45 +0400

    added some responsive elements can be seen in mobile

--- browser/js/SKUinfo/skuController.js
+++ browser/js/SKUinfo/skuController.js
@@ -1,5 +1,5 @@
 app.controller('skuController', function($state, $scope, homeFactory, one_sku){
 	console.log("one_sku:", one_sku)
 		$scope.skuInfo = one_sku;
-		console.log('were here and::-->', $scope.skuInfo);
+		$scope.parentSku = $scope.skuInfo[0].Individual_SKUs
 })
