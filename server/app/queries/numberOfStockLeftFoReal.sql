0de4bae92cc9d82d52da56649cbae7f37a5bcf68
Vivek Pandya <vivek.pandya93@gmail.com>
2015-11-30 17:17:32 +0400

    cerb

--- server/app/queries/numberOfStockLeftFoReal.sql
+++ server/app/queries/numberOfStockLeftFoReal.sql
@@ -2,8 +2,8 @@ select
    size as Size,{{ bobDb }}_ae.catalog_simple.sku as Individual_SKUs,
      {{ cerberus }}.stock_summary.quantity as Available_Stock
       from {{ namdexDb }}.sales_order_item
-     inner join {{ cerberus }}_staging.stock_summary
-      on {{ cerberus }}_staging.stock_summary.simple_sku = {{namdexDb}}.sales_order_item.sku
+   inner join {{ cerberus }}.stock_summary
+on {{ cerberus }}.stock_summary.simple_sku = {{namdexDb}}.sales_order_item.sku
      where {{ namdexDb }}.sales_order_item.product_brand = "{{name}}"
      and {{ namdexDb }}.sales_order_item.status_waterfall = 1
    and status_name <> 'canceled'
