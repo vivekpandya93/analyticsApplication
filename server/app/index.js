7dc3680aef917855aec261a00873c0c42c939cd0
Luciano Colosio <luciano.colosio@namshishi.com>
2015-11-30 13:24:21 +0400

    OUCH, no everybody lucio!!!!

--- server/app/index.js
+++ server/app/index.js
@@ -13,26 +13,26 @@ function getUserByFrontier(req, res, next) {
   var jws = req.cookies['_nmid_secure'] || null;
   var token = (jws) ? JWS.decode(jws) : null;
   req.user = (token) ? token.payload : { email: null, roles: []};
-  req.user = {
-    email: "luciano.colosio@namshi.com",
-      roles: [
-       "user",
-       "admin"
-      ],
-      stuff: "",
-      other_data: {
-       123: true
-      },
-      secure: "",
-      new_stuff: "fafasdfasfd",
-      hash: "",
-      notes: "",
-      bob: "admin",
-      expiry: "",
-      deploy: "zeus, qc",
-      exp: 1447369713,
-      iat: 1447326513
-    };
+  // req.user = {
+  //   email: "luciano.colosio@namshi.com",
+  //     roles: [
+  //      "user",
+  //      "admin"
+  //     ],
+  //     stuff: "",
+  //     other_data: {
+  //      123: true
+  //     },
+  //     secure: "",
+  //     new_stuff: "fafasdfasfd",
+  //     hash: "",
+  //     notes: "",
+  //     bob: "admin",
+  //     expiry: "",
+  //     deploy: "zeus, qc",
+  //     exp: 1447369713,
+  //     iat: 1447326513
+  //   };
 
   next();
 }
