95047115d914872798e6fbb6804fa94d471b6069
Vivek Pandya <vivek.pandya93@gmail.com>
2015-12-01 11:25:46 +0400

    last query fixed - cerberus

--- server/app/queries/numberOfStockLeftFoReal.sql
+++ server/app/queries/numberOfStockLeftFoReal.sql
@@ -1,11 +1,12 @@
 select
-   size as Size, {{namdexDb}}.sales_order_item.sku as Individual_SKUs,
+   	 {{ namdexDb }}.sales_order_item.size as Size, 
+   	 {{namdexDb}}.sales_order_item.sku as Individual_SKUs,
      {{ cerberus }}.stock_summary.quantity as Available_Stock
-      from {{ namdexDb }}.sales_order_item
-   inner join {{ cerberus }}.stock_summary
-on {{ cerberus }}.stock_summary.simple_sku = {{namdexDb}}.sales_order_item.sku
+from {{ namdexDb }}.sales_order_item
+     inner join {{ cerberus }}.stock_summary
+		 on {{ cerberus }}.stock_summary.simple_sku = {{namdexDb}}.sales_order_item.sku
      where {{ namdexDb }}.sales_order_item.product_brand = "{{name}}"
      and {{ namdexDb }}.sales_order_item.status_waterfall = 1
-   and status_name <> 'canceled'
-     and sku_config = "{{sku}}"
+     and {{ namdexDb }}.sales_order_item.status_name <> 'canceled'
+     and {{ namdexDb }}.sales_order_item.sku_config = "{{sku}}"
      group by Individual_SKUs
