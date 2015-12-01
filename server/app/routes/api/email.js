9290ca951cd96006e42101331944e7271af87b75
Luciano Colosio <luciano.colosio@namshishi.com>
2015-12-01 10:25:07 +0400

    Debug karl

--- server/app/routes/api/email.js
+++ server/app/routes/api/email.js
@@ -1,16 +1,19 @@
 var router = require('express').Router();
 var request = require('superagent');
 var config = require('../../../config');
+var url = config.get('apis.karl.emailAllert)');
+var account = config.get('apps.karl');
 
 router.post('/', function (req, res) {
-	console.log("creds", req.body)
+	console.log('creds: ', req.body);
 	var from = req.user.email;
 	var to = req.body.recepient;
 	var message = req.body.message;
 	var email = {recipient: to, message: message, subject: 'You have a new message from ' + from };
+  console.log('seding email to karl' + url + ' with account: ', JSON.stringify(account));
 
-	request.post(config.get('apis.karl.emailAllert)'))
-		.auth('tech_user', 'tech1234')
+	request.post(url)
+		.auth(account.user, account.password)
 		.set('Content-Type','application/json')
 		.accept('application/json')
 		.send(email)
