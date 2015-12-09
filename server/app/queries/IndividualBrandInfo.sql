ae1bb3dc7874487d00d89d8e4c3441f182d49e39
Vivek Pandya <vivek.pandya93@gmail.com>
2015-12-09 14:21:55 +0400

    namshi.net

--- server/app/queries/IndividualBrandInfo.sql
+++ server/app/queries/IndividualBrandInfo.sql
@@ -1,51 +1,51 @@
-select  {{ namdexDb }}.sales_order_item.product_name as Name,
-				{{ namdexDb }}.sales_order_item.sku_config as SKU,
-				sum({{ namdexDb }}.sales_order_item.paid_price) as Revenue,
+select  {{ locales.namdexDB }}.sales_order_item.product_name as Name,
+				{{ locales.namdexDB }}.sales_order_item.sku_config as SKU,
+				sum({{ locales.namdexDB }}.sales_order_item.paid_price) as Revenue,
   			count(*) as UnitsSold
-from {{ namdexDb }}.sales_order_item
+from {{ locales.namdexDB }}.sales_order_item
 
-left join {{ bobDb }}_ae.catalog_config
-on {{ namdexDb }}.sales_order_item.sku_config = {{ bobDb }}_ae.catalog_config.sku
+left join {{ locales.bobDb }}_ae.catalog_config
+on {{ locales.namdexDB }}.sales_order_item.sku_config = {{ locales.bobDb }}_ae.catalog_config.sku
 
-left join {{ bobDb }}_ae.catalog_attribute_option_global_department
-on {{ bobDb }}_ae.catalog_attribute_option_global_department.id_catalog_attribute_option_global_department = {{ bobDb }}_ae.catalog_config.fk_catalog_attribute_option_global_department
+left join {{ locales.bobDb }}_ae.catalog_attribute_option_global_department
+on {{ locales.bobDb }}_ae.catalog_attribute_option_global_department.id_catalog_attribute_option_global_department = {{ locales.bobDb }}_ae.catalog_config.fk_catalog_attribute_option_global_department
 
-left join {{ bobDb }}_ae.catalog_attribute_option_global_gender_new
-on {{ bobDb }}_ae.catalog_attribute_option_global_gender_new.id_catalog_attribute_option_global_gender_new = {{ bobDb }}_ae.catalog_config.fk_catalog_attribute_option_global_gender_new
+left join {{ locales.bobDb }}_ae.catalog_attribute_option_global_gender_new
+on {{ locales.bobDb }}_ae.catalog_attribute_option_global_gender_new.id_catalog_attribute_option_global_gender_new = {{ locales.bobDb }}_ae.catalog_config.fk_catalog_attribute_option_global_gender_new
 
-left join {{ bobDb }}_ae.catalog_attribute_option_global_category
-on {{ bobDb }}_ae.catalog_attribute_option_global_category.id_catalog_attribute_option_global_category = {{ bobDb }}_ae.catalog_config.fk_catalog_attribute_option_global_category
+left join {{ locales.bobDb }}_ae.catalog_attribute_option_global_category
+on {{ locales.bobDb }}_ae.catalog_attribute_option_global_category.id_catalog_attribute_option_global_category = {{ locales.bobDb }}_ae.catalog_config.fk_catalog_attribute_option_global_category
 
-WHERE {{ namdexDb }}.sales_order_item.ordered_at between {% bind from %} and {% bind to %}
+WHERE {{ locales.namdexDB }}.sales_order_item.ordered_at between {% bind variables.from %} and {% bind variables.to %}
 
 
 and status_waterfall = 1
-and {{ namdexDb }}.sales_order_item.product_brand = {% bind spaced_name %}
+and {{ locales.namdexDB }}.sales_order_item.product_brand = {% bind variables.spaced_name %}
 and status_name not in ('canceled','test_invalid')
 
 {% if department %}
-	and {{ bobDb }}_ae.catalog_attribute_option_global_department.name = {% bind department %}
+	and {{ locales.bobDb }}_ae.catalog_attribute_option_global_department.name = {% bind variables.department %}
 {% else %}
-	and {{ bobDb }}_ae.catalog_attribute_option_global_department.name in ('Accessories', 'Bags', 'Clothing', 'Shoes')
+	and {{ locales.bobDb }}_ae.catalog_attribute_option_global_department.name in ('Accessories', 'Bags', 'Clothing', 'Shoes')
 {%endif%}
 
 {% if gender %}
-and (case when {{ namdexDb }}.sales_order_item.product_age_group <> "Adult" && {{ namdexDb }}.sales_order_item.product_gender = "Male" then "Boy"
-		  when {{ namdexDb }}.sales_order_item.product_age_group <> "Adult" && {{ namdexDb }}.sales_order_item.product_gender ="Female" then "Girl"
-     else {{ namdexDb }}.sales_order_item.product_gender end) = {% bind gender %}
+and (case when {{ locales.namdexDB }}.sales_order_item.product_age_group <> "Adult" && {{ locales.namdexDB }}.sales_order_item.product_gender = "Male" then "Boy"
+		  when {{ locales.namdexDB }}.sales_order_item.product_age_group <> "Adult" && {{ locales.namdexDB }}.sales_order_item.product_gender ="Female" then "Girl"
+     else {{ locales.namdexDB }}.sales_order_item.product_gender end) = {% bind variables.gender %}
 
 {% else %}
-		and (case when {{ namdexDb }}.sales_order_item.product_age_group <> "Adult" && {{ namdexDb }}.sales_order_item.product_gender = "Male" then "Boy"
-		 when {{ namdexDb }}.sales_order_item.product_age_group <> "Adult" && {{ namdexDb }}.sales_order_item.product_gender ="Female" then "Girl"
-     else {{ namdexDb }}.sales_order_item.product_gender end) in ('Boy' , 'Girl', 'Male', 'Female')
+		and (case when {{ locales.namdexDB }}.sales_order_item.product_age_group <> "Adult" && {{ locales.namdexDB }}.sales_order_item.product_gender = "Male" then "Boy"
+		 when {{ locales.namdexDB }}.sales_order_item.product_age_group <> "Adult" && {{ locales.namdexDB }}.sales_order_item.product_gender ="Female" then "Girl"
+     else {{ locales.namdexDB }}.sales_order_item.product_gender end) in ('Boy' , 'Girl', 'Male', 'Female')
 {% endif %}
 
 {% if category %}
-	and {{ bobDb }}_ae.catalog_attribute_option_global_category.name = {% bind category %}
+	and {{ locales.bobDb }}_ae.catalog_attribute_option_global_category.name = {% bind variables.category %}
 {%endif%}
 
-and {{ namdexDb }}.sales_order_item.status_waterfall = 1
-group by {{ namdexDb }}.sales_order_item.sku_config, {{ namdexDb }}.sales_order_item.product_name
+and {{ locales.namdexDB }}.sales_order_item.status_waterfall = 1
+group by {{ locales.namdexDB }}.sales_order_item.sku_config, {{ locales.namdexDB }}.sales_order_item.product_name
 order by Revenue desc
 limit 250;
 
--- server/app/queries/number.sql
+++ server/app/queries/number.sql
@@ -1,11 +1,11 @@
 select
-   	 {{ namdexDb }}.sales_order_item.size as Size, 
-   	 {{ namdexDb }}.sales_order_item.sku as Individual_SKUs,
-     {{ cerberus }}.stock_summary.quantity as Available_Stock
-from {{ namdexDb }}.sales_order_item
-     left join {{ cerberus }}.stock_summary
-		 on {{ cerberus }}.stock_summary.simple_sku = {{ namdexDb }}.sales_order_item.sku
-     where {{ namdexDb }}.sales_order_item.status_waterfall = 1
-     and {{ namdexDb }}.sales_order_item.status_name <> 'canceled'
-     and {{ namdexDb }}.sales_order_item.sku_config = {% bind sku %}
+   	 {{ locales.namdexDB }}.sales_order_item.size as Size, 
+   	 {{ locales.namdexDB }}.sales_order_item.sku as Individual_SKUs,
+     {{ locales.cerberus }}.stock_summary.quantity as Available_Stock
+from {{ locales.namdexDB }}.sales_order_item
+     left join {{ locales.cerberus }}.stock_summary
+		 on {{ locales.cerberus }}.stock_summary.simple_sku = {{ locales.namdexDB }}.sales_order_item.sku
+     where {{ locales.namdexDB }}.sales_order_item.status_waterfall = 1
+     and {{ locales.namdexDB }}.sales_order_item.status_name <> 'canceled'
+     and {{ locales.namdexDB }}.sales_order_item.sku_config = {% bind variables.sku %}
      group by Individual_SKUs
