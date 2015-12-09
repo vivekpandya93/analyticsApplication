260d65b395d905b9590c100b0f3cce2eb49133f7
Vivek Pandya <vivek.pandya93@gmail.com>
2015-12-09 14:34:51 +0400

    namshi.net

--- server/app/queries/number.sql
+++ server/app/queries/number.sql
@@ -1,11 +1,11 @@
 select
-   	 {{ locales.namdexDB }}.sales_order_item.size as Size, 
-   	 {{ locales.namdexDB }}.sales_order_item.sku as Individual_SKUs,
+   	 {{ locales.namdexDb }}.sales_order_item.size as Size, 
+   	 {{ locales.namdexDb }}.sales_order_item.sku as Individual_SKUs,
      {{ locales.cerberus }}.stock_summary.quantity as Available_Stock
-from {{ locales.namdexDB }}.sales_order_item
+from {{ locales.namdexDb }}.sales_order_item
      left join {{ locales.cerberus }}.stock_summary
-		 on {{ locales.cerberus }}.stock_summary.simple_sku = {{ locales.namdexDB }}.sales_order_item.sku
-     where {{ locales.namdexDB }}.sales_order_item.status_waterfall = 1
-     and {{ locales.namdexDB }}.sales_order_item.status_name <> 'canceled'
-     and {{ locales.namdexDB }}.sales_order_item.sku_config = {% bind variables.sku %}
+		 on {{ locales.cerberus }}.stock_summary.simple_sku = {{ locales.namdexDb }}.sales_order_item.sku
+     where {{ locales.namdexDb }}.sales_order_item.status_waterfall = 1
+     and {{ locales.namdexDb }}.sales_order_item.status_name <> 'canceled'
+     and {{ locales.namdexDb }}.sales_order_item.sku_config = {% bind variables.sku %}
      group by Individual_SKUs
