3c51e230dc30f3a3293be3f42d26237326e48bd2
Vivek Pandya <vivek.pandya93@gmail.com>
2015-12-22 17:50:47 +0400

    css

--- server/app/queries/revenue.sql
+++ server/app/queries/revenue.sql
@@ -41,6 +41,6 @@ and (case when {{ locales.namdexDb }}.sales_order_item.product_age_group <> "Adu
 
 and {{ locales.namdexDb }}.sales_order_item.status_waterfall = 1
 group by product_brand
-order by Revenue desc
+order by UnitsSold desc
 limit 500;
 
